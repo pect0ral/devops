1a9461b457ddf1a4be658e8a63a4e75a637b2813
root <root@DevOps-Puppet-Master.(none)>
2016-09-30 02:12:18 +0000

    Added a webserver

---
+++ site/profile/manifests/webserver.pp
@@ -0,0 +1,15 @@
+class profile::webserver {
+	$index_content = "<html>
+  	<head>
+		<title>Hello World</title> </head>
+	<body>
+		<h1>Hello World!</h1>
+  	</body>		
+	</html>"
+
+	file {
+		path => '/var/www/html/index.html',
+		content => $index_content,
+		mode => 0644,
+	}		
+}
